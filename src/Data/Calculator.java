e3e4b6b09480457d9ea9dead8019da837bbd63e5
Wilson Fernando Antury Torres <wilson.antury@globant.com>
2022-04-20 15:45:07 -0500

    [Add] base calculator with sum and substract methods

---
+++ src/Data/Calculator.java
@@ -0,0 +1,12 @@
+package Data;
+
+public class Calculator {
+
+    public float sum(float x, float y){
+        return x + y;
+    }
+
+    public float substract(float x, float y){
+        return  x - y;
+    }
+}
